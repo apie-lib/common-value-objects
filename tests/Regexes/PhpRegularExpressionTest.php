0c7aa0c75d60ebe4ccfd28627732d4605005b1d6
Pieter <pieter_jordaan@hotmail.com>
2022-06-25 12:07:59 +0000

    add PhpRegularExpression class

---
+++ tests/Regexes/PhpRegularExpressionTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Apie\Tests\CommonValueObjects\Regexes;
+
+use Apie\CommonValueObjects\Exceptions\InvalidPhpRegularExpression;
+use Apie\CommonValueObjects\Regexes\PhpRegularExpression;
+use Apie\CommonValueObjects\Texts\NonEmptyString;
+use Apie\Core\ValueObjects\Exceptions\InvalidStringForValueObjectException;
+use PHPUnit\Framework\TestCase;
+
+class PhpRegularExpressionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider inputProvider
+     */
+    public function it_allows_valid_regular_expressions(string $expected, string $input)
+    {
+        $testItem = new PhpRegularExpression($input);
+        $this->assertEquals($expected, $testItem->toNative());
+    }
+
+    public function inputProvider()
+    {
+        yield ['/test/i', '/test/i'];
+        yield ['/test/', '/test/'];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidProvider
+     */
+    public function it_refuses_invalid_regular_expressions(string $input)
+    {
+        $this->expectException(InvalidPhpRegularExpression::class);
+        new PhpRegularExpression($input);
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidProvider
+     */
+    public function it_refuses_invalid_regular_expressions_with_fromNative(string $input)
+    {
+        $this->expectException(InvalidPhpRegularExpression::class);
+        PhpRegularExpression::fromNative($input);
+    }
+
+    public function invalidProvider()
+    {
+        yield [''];
+        yield ['[a-z]'];
+        yield ["/[a-z]"];
+        yield ['/[a-z]/0'];
+    }
+}
--- tests/Texts/NonEmptyStringTest.php
+++ tests/Texts/NonEmptyStringTest.php
@@ -2,10 +2,23 @@
 namespace Apie\Tests\CommonValueObjects\Texts;
 
 use Apie\CommonValueObjects\Texts\NonEmptyString;
+use Apie\Core\ValueObjects\Exceptions\InvalidStringForValueObjectException;
+use Apie\Fixtures\TestsHelpers\TestWithFaker;
 use PHPUnit\Framework\TestCase;
 
 class NonEmptyStringTest extends TestCase
 {
+    use TestWithFaker;
+    /**
+     * @test
+     * @dataProvider inputProvider
+     */
+    public function fromNative_allows_all_strings_that_are_not_empty(string $expected, string $input)
+    {
+        $testItem = NonEmptyString::fromNative($input);
+        $this->assertEquals($expected, $testItem->toNative());
+    }
+
     /**
      * @test
      * @dataProvider inputProvider
@@ -20,5 +33,40 @@ class NonEmptyStringTest extends TestCase
     {
         yield ['test', 'test'];
         yield ['trimmed', '   trimmed   '];
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidProvider
+     */
+    public function it_refuses_empty_strings(string $input)
+    {
+        $this->expectException(InvalidStringForValueObjectException::class);
+        new NonEmptyString($input);
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidProvider
+     */
+    public function it_refuses_empty_strings_with_fromNative(string $input)
+    {
+        $this->expectException(InvalidStringForValueObjectException::class);
+        NonEmptyString::fromNative($input);
+    }
+
+    public function invalidProvider()
+    {
+        yield [''];
+        yield [' '];
+        yield ["          \t\n\r\n"];
+    }
+
+    /**
+     * @test
+     */
+    public function it_works_with_apie_faker()
+    {
+        $this->runFakerTest(NonEmptyString::class);
     }
 }
