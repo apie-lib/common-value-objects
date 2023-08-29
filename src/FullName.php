dbab6d507cf859c8b294880b039308d727143a5c
pjordaan <pjordaan@users.noreply.github.com>
2023-08-29 13:26:59 +0000

    Update file(s) "packages/common-value-objects/." from "apie-lib/apie-lib-monorepo"

--- src/FullName.php
+++ src/FullName.php
@@ -2,8 +2,10 @@
 namespace Apie\CommonValueObjects;
 
 use Apie\Core\Attributes\FakeMethod;
+use Apie\Core\Attributes\ProvideIndex;
 use Apie\Core\ValueObjects\CompositeValueObject;
 use Apie\Core\ValueObjects\Interfaces\ValueObjectInterface;
+use Apie\CountWords\WordCounter;
 use Apie\TextValueObjects\FirstName;
 use Apie\TextValueObjects\LastName;
 use Faker\Generator;
@@ -13,6 +15,7 @@ use Stringable;
  * Represents a full name: gender + first name + last name.
  */
 #[FakeMethod('createRandom')]
+#[ProvideIndex('getIndexes')]
 class FullName implements ValueObjectInterface, Stringable
 {
     use CompositeValueObject;
@@ -27,6 +30,14 @@ class FullName implements ValueObjectInterface, Stringable
     public function __toString(): string
     {
         return $this->gender->getSalutation() . ' ' . $this->firstName . ' '  . $this->lastName;
+    }
+
+    /**
+     * @return array<string, int>
+     */
+    public function getIndexes(): array
+    {
+        return WordCounter::countFromString($this->__toString());
     }
 
     public static function createRandom(Generator $generator): self
