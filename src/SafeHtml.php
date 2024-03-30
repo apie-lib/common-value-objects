fa0152f60d96f9892e336423b37a48f01b51e33d
pjordaan <pjordaan@users.noreply.github.com>
2024-03-30 07:47:48 +0000

    Update file(s) "packages/common-value-objects/." from "apie-lib/apie-lib-monorepo"

---
+++ src/SafeHtml.php
@@ -0,0 +1,75 @@
+<?php
+namespace Apie\CommonValueObjects;
+
+use Apie\Core\Attributes\FakeMethod;
+use Apie\Core\Attributes\ProvideIndex;
+use Apie\Core\ValueObjects\Exceptions\InvalidStringForValueObjectException;
+use Apie\Core\ValueObjects\Interfaces\StringValueObjectInterface;
+use Apie\Core\ValueObjects\IsStringValueObject;
+use Apie\CountWords\WordCounter;
+use Faker\Generator;
+use ReflectionClass;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+
+#[FakeMethod('createRandom')]
+#[ProvideIndex('provideIndexes')]
+final class SafeHtml implements StringValueObjectInterface
+{
+    use IsStringValueObject;
+
+    private static HtmlSanitizer $htmlSanitizer;
+
+    private static function getHtmlSanitizer(): HtmlSanitizer
+    {
+        if (!isset(self::$htmlSanitizer)) {
+            $config = (new HtmlSanitizerConfig())
+                ->allowSafeElements()
+                ->dropAttribute('id', [])
+                ->forceAttribute('a', 'rel', 'noopener noreferrer')
+                ->forceAttribute('a', 'target', '_blank')
+                ->allowLinkSchemes(['https', 'http', 'mailto'])
+                ->allowRelativeLinks(false)
+                ->withMaxInputLength(70000) // character limit is 65535, but maybe HTML is removed
+                ->dropElement('html')
+                ->dropElement('head')
+                ->dropElement('script')
+                ->dropElement('style');
+            self::$htmlSanitizer = new HtmlSanitizer(
+                $config
+            );
+        }
+
+        return self::$htmlSanitizer;
+    }
+
+    public static function createRandom(Generator $factory): self
+    {
+        $randomHtmls = ['div', 'h1', 'h2', 'h3', 'p'];
+        $string = '<div>';
+        $counter = $factory->numberBetween(1, 3);
+        for ($i = 0; $i < $counter; $i++) {
+            $tag = $factory->randomElement($randomHtmls);
+            $string .= '<' . $tag . '>' . $factory->text(50) . '</' . $tag . '>';
+        }
+        $string .= '</div>';
+        return new self($string);
+    }
+
+    public function provideIndexes(): array
+    {
+        return WordCounter::countFromString(strip_tags(str_replace('<', ' <', $this->internal)));
+    }
+
+    public static function validate(string $input): void
+    {
+        if (strlen($input) > 65535) {
+            throw new InvalidStringForValueObjectException($input, new ReflectionClass(__CLASS__));
+        }
+    }
+
+    protected function convert(string $input): string
+    {
+        return self::getHtmlSanitizer()->sanitizeFor('body', $input);
+    }
+}
---
+++ tests/SafeHtmlTest.php
@@ -0,0 +1,99 @@
+<?php
+namespace Apie\CommonValueObjects;
+
+use Apie\Core\ValueObjects\Exceptions\InvalidStringForValueObjectException;
+use Apie\Fixtures\TestHelpers\TestWithFaker;
+use Apie\Fixtures\TestHelpers\TestWithOpenapiSchema;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class SafeHtmlTest extends TestCase
+{
+    use TestWithOpenapiSchema;
+    use TestWithFaker;
+
+    /**
+     * @test
+     */
+    public function it_works_with_schema_generator()
+    {
+        $this->runOpenapiSchemaTestForCreation(
+            SafeHtml::class,
+            'SafeHtml-post',
+            [
+                'type' => 'string',
+                'format' => 'safehtml',
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_refuses_large_html()
+    {
+        $this->expectException(InvalidStringForValueObjectException::class);
+        new SafeHtml(str_repeat('aaa', 65536));
+    }
+
+    /**
+     * @test
+     * @dataProvider invalidHTMLProvider
+     */
+    public function it_sanitizes_html(string $expected, string $input)
+    {
+        $testItem = new SafeHtml($input);
+        $this->assertEquals($expected, $testItem->toNative());
+    }
+
+    public function invalidHTMLProvider(): Generator
+    {
+        yield 'malformed HTML' => [
+            'this is a test<div>hi</div>',
+            'this is a test<div>hi'
+        ];
+        yield 'script tag' => [
+            'test',
+            '<script>alert("hi");</script>test'
+        ];
+        yield 'img onerror XSS' => [
+            '<img />',
+            '<img src="error" onerror="alert(\'hi\');" />'
+        ];
+        yield 'external urls open in new window' => [
+            '<a href="https://www.google.com" rel="noopener noreferrer" target="_blank">google.com</a>',
+            '<a href="https://www.google.com">google.com</a>',
+        ];
+        yield 'script + text truncating' => [
+            '',
+            '<script>alert("HI");//' . str_repeat('a', 70000) . PHP_EOL . '</script>'
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_indexes_html()
+    {
+        $testItem = new SafeHtml('<div>Test</div><p>Paragraph</p><h1>Header is important, right?</h1>');
+        $this->assertEquals(
+            [
+                'test' => 1,
+                'paragraph' => 1,
+                'header' => 1,
+                'is' => 1,
+                'important' => 1,
+                'right' => 1,
+            ],
+            $testItem->provideIndexes()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_works_with_apie_faker()
+    {
+        $this->runFakerTest(SafeHtml::class);
+    }
+}
