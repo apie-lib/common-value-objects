fcad078fa9881e049d1524e575ddd92253dbf6a0
pjordaan <pjordaan@users.noreply.github.com>
2025-12-01 05:37:39 +0000

    Update file(s) "packages/common-value-objects/." from "apie-lib/apie-lib-monorepo"

--- tests/SafeHtmlTest.php
+++ tests/SafeHtmlTest.php
@@ -84,7 +84,8 @@ class SafeHtmlTest extends TestCase
             '<div>&nbsp;</div>'
         ];
         yield 'Exploitable wrong html' => [
-            '<div></div>',
+            // this no longer works on PHP 8.4 because of the new native parser.
+            PHP_VERSION_ID >= 80400 ? "" : '<div></div>',
             '<div onload="xss()"></div>',
         ];
         yield 'Youtube movie' => [
